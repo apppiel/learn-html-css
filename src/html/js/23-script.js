51bdc6cef403961a7cc4dda9ded45c2605a52f17
apppiel <c_j-_-@naver.com>
2025-02-19 14:18:12 +0900

    script 요소

---
+++ src/html/js/23-script.js
@@ -0,0 +1,9 @@
+// DOM Selection
+const flipButton = document.querySelector('.flip-button');
+const card = document.querySelector('.card');
+
+// Function Implementation
+const flipCard = () => card.classList.toggle('is-flipped');
+
+// Event Binding
+flipButton.addEventListener('click', flipCard);
